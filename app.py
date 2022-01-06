b7ea667a3d986f93fad4cd2fc2f90b31675005b7
prmohapatra <pratyush.mohapatra@rabobank.nl>
2022-01-06 21:00:27 +0100

    added

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, url_for
+from azure.identity import DefaultAzureCredential
+from azure.storage.filedatalake import DataLakeServiceClient
+import json
+import csv
+import pandas as pd
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    
+    data = '{ "name":"John", "age":30, "city":"New York"}'
+    y = json.loads(data)
+    print (y)
+    return y
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
