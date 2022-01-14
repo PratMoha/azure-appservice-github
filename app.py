16a42adf1d7bff68ba028815dec7c4a487de7b1c
prmohapatra <pratyush.mohapatra@rabobank.nl>
2022-01-14 13:02:49 +0100

    added

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     
     credentials = ClientSecretCredential("a572004d-6f4c-4446-824a-c5a9cac4ff21", "5a54f6b8-a6ac-4dd9-aec6-7cf8063953f6", "xkG7Q~8Xg0C5~hkjZ.pgCvPYJ8MdqoJZlhvxQ")
     print(credentials.get_token())
-    data = '{ "name":"Pratyush", "age":30, "city":"New York"}'
+    data = '{ "name":"Pratyush", "age":300, "city":"New York"}'
     y = json.loads(data)
     print (y)
     return y
