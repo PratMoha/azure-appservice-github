a04b1f52285f55436094d48557cd80194c657412
prmohapatra <pratyush.mohapatra@rabobank.nl>
2022-01-14 14:18:11 +0100

    added

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    
-    credentials = ClientSecretCredential("a572004d-6f4c-4446-824a-c5a9cac4ff21", "5a54f6b8-a6ac-4dd9-aec6-7cf8063953f6", "xkG7Q~8Xg0C5~hkjZ.pgCvPYJ8MdqoJZlhvxQ")
-    print(credentials.get_token())
-    data = '{ "name":"Pratyush", "age":300, "city":"New York"}'
-    y = json.loads(data)
-    print (y)
-    return y
+    try:
+        print("Inside code")
+        credentials = ClientSecretCredential("a572004d-6f4c-4446-824a-c5a9cac4ff21", "5a54f6b8-a6ac-4dd9-aec6-7cf8063953f6", "xkG7Q~8Xg0C5~hkjZ.pgCvPYJ8MdqoJZlhvxQ")
+        print(credentials._client_id)
+        data = '{ "name":"Pratyush", "age":300, "city":"New York"}'
+        y = json.loads(data)
+        print (y)
+        return credentials._client_id
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
